34355c06a67c9faf10a91471db681fd1e3391384
Afia Islam <iafia9742@gmail.com>
2024-08-22 22:20:01 +0600

    Rename file

--- Pattern_R.py
+++ Pattern_R.py
@@ -10,18 +10,18 @@ Pattern 18:
 n=int(input("Enter Odd Row no.: "))
 
 mid=int(n/2+1)
-stop=n-1
-start=1
+sp=n-1
+st=1
 for i in range(1,n+1):
-    for j in range(1,stop):
+    for j in range(1,sp):
         print(" ",end=" ")
-    for k in range(1,start+1):
+    for k in range(1,st+1):
         print("*",end=" ")
     
     if i<mid:
-        stop-=1
-        start+=2
+        sp-=1
+        st+=2
     else:
-        stop+=1
-        start-=2
+        sp+=1
+        st-=2
     print()
